ee096c45d75a4ba09f4bf6eae167ad561051c19e
Bang-tv259 <123390345+Bang-tv259@users.noreply.github.com>
2023-01-29 22:28:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+let container1;
+let container2;
+let container3;
+let container4;
+let container5;
+let container6;
+let count;
+
+container1 = document.getElementById('container1');
+count = 50;
+for(var i = 0; i<50; i++){
+    let leftSnow = Math.floor(Math.random() * container1.clientWidth);
+    let topSnow = Math.floor(Math.random() * container1.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 50);
+    let timeSnow = Math.floor((Math.random() * 5) + 5);
+    let blurSnow = Math.floor(Math.random() * 10);
+    console.log(leftSnow);
+    let div = document.createElement('div');
+    div.classList.add('snow');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    container1.appendChild(div);
+}
+
+container2 = document.getElementById('container2');
+count = 50;
+for(var i = 0; i<50; i++){
+    let leftSnow = Math.floor(Math.random() * container2.clientWidth);
+    let topSnow = Math.floor(Math.random() * container2.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 50);
+    let timeSnow = Math.floor((Math.random() * 5) + 5);
+    let blurSnow = Math.floor(Math.random() * 10);
+    console.log(leftSnow);
+    let div = document.createElement('div');
+    div.classList.add('snow');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    container2.appendChild(div);
+}
+
+container3 = document.getElementById('container3');
+count = 50;
+for(var i = 0; i<50; i++){
+    let leftSnow = Math.floor(Math.random() * container3.clientWidth);
+    let topSnow = Math.floor(Math.random() * container3.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 50);
+    let timeSnow = Math.floor((Math.random() * 5) + 5);
+    let blurSnow = Math.floor(Math.random() * 10);
+    console.log(leftSnow);
+    let div = document.createElement('div');
+    div.classList.add('snow');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    container3.appendChild(div);
+}
+
+container4 = document.getElementById('container4');
+count = 50;
+for(var i = 0; i<50; i++){
+    let leftSnow = Math.floor(Math.random() * container4.clientWidth);
+    let topSnow = Math.floor(Math.random() * container4.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 50);
+    let timeSnow = Math.floor((Math.random() * 5) + 5);
+    let blurSnow = Math.floor(Math.random() * 10);
+    console.log(leftSnow);
+    let div = document.createElement('div');
+    div.classList.add('snow');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    container4.appendChild(div);
+}
+
+container5 = document.getElementById('container5');
+count = 50;
+for(var i = 0; i<50; i++){
+    let leftSnow = Math.floor(Math.random() * container5.clientWidth);
+    let topSnow = Math.floor(Math.random() * container5.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 50);
+    let timeSnow = Math.floor((Math.random() * 5) + 5);
+    let blurSnow = Math.floor(Math.random() * 10);
+    console.log(leftSnow);
+    let div = document.createElement('div');
+    div.classList.add('snow');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    container5.appendChild(div);
+}
+
+container6 = document.getElementById('container6');
+count = 50;
+for(var i = 0; i<50; i++){
+    let leftSnow = Math.floor(Math.random() * container6.clientWidth);
+    let topSnow = Math.floor(Math.random() * container6.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 50);
+    let timeSnow = Math.floor((Math.random() * 5) + 5);
+    let blurSnow = Math.floor(Math.random() * 10);
+    console.log(leftSnow);
+    let div = document.createElement('div');
+    div.classList.add('snow');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    container6.appendChild(div);
+}
